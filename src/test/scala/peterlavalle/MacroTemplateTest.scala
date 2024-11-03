fc22361142ac6d1c117c1324d4b45c362e7d6a49
Peter LaValle <peter.lavalle@gmail.com>
2024-11-03 00:28:19 +0000

    added notes tot he test

--- src/test/scala/peterlavalle/MacroTemplateTest.scala
+++ src/test/scala/peterlavalle/MacroTemplateTest.scala
@@ -5,12 +5,18 @@ class MacroTemplateTest extends munit.FunSuite {
 	test("so now what") {
 
 		class Donk extends ResourceTemplate.Macro {
-			def bar() = template
+			def bar() =
+				// with no extension this reads "Donk.bar"as the template source
+				// https://github.com/g-pechorin/resource-template/blob/default/src/test/scala/peterlavalle/Donk.bar
+				template
 
 			def foo = 8
 			
 
-			def bar(name: String) = template("txt")
+			def bar(name: String) =
+				// this one uses a different extension ... so i can have lots of matching methods for reasons
+				// ths file is at https://github.com/g-pechorin/resource-template/blob/default/src/test/scala/peterlavalle/Donk.bar.txt
+				template("txt")
 		}
 
 		assertEquals(
